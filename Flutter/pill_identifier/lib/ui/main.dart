a51530d54ef88e38e891a80655a7956e884ee941
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-04-24 22:21:28 +0700

    Firt commit on crypto-wallet
    Firebase setup

--- Flutter/pill_identifier/lib/ui/main.dart
+++ Flutter/pill_identifier/lib/ui/main.dart
@@ -3,8 +3,16 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:pill_identifier/ui/authentication.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pill_identifier/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: Authentication(),
     );
   }
 }
