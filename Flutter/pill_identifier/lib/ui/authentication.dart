d99d12220ea364d3132e6d8562bf723e9a449033
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-04-25 04:18:54 +0700

    Update UI

--- Flutter/pill_identifier/lib/ui/authentication.dart
+++ Flutter/pill_identifier/lib/ui/authentication.dart
@@ -18,51 +18,54 @@ class _AuthenticationState extends State<Authentication> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(color: Colors.blueAccent),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-                controller: _emailField,
-                decoration: const InputDecoration(
-                  hintText: "something@email.com",
-                  hintStyle: TextStyle(color: Colors.white),
-                  labelText: "Email",
-                  labelStyle: TextStyle(color: Colors.white),
-                )),
-            TextFormField(
-                controller: _passwordField,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "password",
-                  hintStyle: TextStyle(color: Colors.white),
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.white),
-                )),
-            Container(
-              width: MediaQuery.of(context).size.width / 1.4,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                  controller: _emailField,
+                  decoration: const InputDecoration(
+                    hintText: "something@email.com",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.white),
+                  )),
+              TextFormField(
+                  controller: _passwordField,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "password",
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white),
+                  )),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.4,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white,
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text("Register"),
+                ),
               ),
-              child: MaterialButton(
-                onPressed: () {},
-                child: const Text("Register"),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.4,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white,
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text("Log in"),
+                ),
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width / 1.4,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: Colors.white,
-              ),
-              child: MaterialButton(
-                onPressed: () {},
-                child: const Text("Log in"),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
